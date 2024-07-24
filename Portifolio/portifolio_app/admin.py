f2a2cce0e7e7316f03abcb06266be2c3e1ef6f39
eltonMessiass <eltonmessias10@gmail.com>
2024-07-24 09:04:36 +0200

    wednesday

--- Portifolio/portifolio_app/admin.py
+++ Portifolio/portifolio_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Tools
+
+
+admin.site.register(Project)
+admin.site.register(Tools)
+
--- Portifolio/portifolio_app/models.py
+++ Portifolio/portifolio_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Tools(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    tools = models.ManyToManyField(Tools, null=True, blank=True)
+    github = models.CharField(null=True, blank=True)
+    view_live = models.CharField(null=True, blank=True)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
